21ec86a10cc25dd47a52b8f92a6358108d5e75b3
Natii <Natii@DESKTOP-7K17A8A>
2018-11-20 00:44:07 +0100

    Gettersy Setters en la clase Caballo

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -2,7 +2,23 @@ package org.iesalandalus.programacion.caballoajedrez;
 
 public class Caballo {
 	
+	// Variables
 	private Color color;
-	private Posicion posicion;
+	private Posicion posicion;	
+	
+	// Getters y Setters
+	
+	public Color getColor() {
+		return color;
+	}
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	public void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	}
 
 }
