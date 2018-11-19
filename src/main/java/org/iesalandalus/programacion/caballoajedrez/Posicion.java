e4feabee50feb31e32755c6995190fa945a01f19
Natii <Natii@DESKTOP-7K17A8A>
2018-11-19 19:38:28 +0100

    Metodos get y set. Validar datos y lanzar excepción

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -4,5 +4,30 @@ public class Posicion {
 	
 	private int fila;
 	private char columna;
+	
+	public int getFila() {
+		return fila;
+	}
+	public void setFila(int fila) throws IllegalArgumentException {
+		if(fila >= 1 && fila <= 8 ) {
+			this.fila = fila;
+		} else {
+			 throw new IllegalArgumentException("Número fuera del tablero");
+		}
+		
+	}
+	public char getColumna() {
+		return columna;
+	}
+	public void setColumna(char columna) throws IllegalArgumentException {
+		if(columna >= 'a' && columna <= 'h') {
+			this.columna = columna;	
+		} else {
+			 throw new IllegalArgumentException("Letras fuera del tablero");
+		}
+		
+	}
+	
+	
 
 }
